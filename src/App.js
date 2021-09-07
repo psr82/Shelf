9813fef172b4b73a6c83a866bb49f99b309c8049
paras <rajputparas.99@gmail.com>
2021-09-07 11:09:12 +0530

    update 1

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Landing from './components/Landing';
+
+function App() {
+
+  return (
+    <div className="App">
+      <div>
+        <Landing/>
+      </div>
+    </div>
+  );
+}
+
+export default App;
